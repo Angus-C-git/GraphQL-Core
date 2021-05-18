437090123b8d46ecbfa65215a0ae8788b35e36d9
Zephyr <anguscornall@gmail.com>
2021-05-19 00:31:15 +1000

    UPDATED: login errors

--- src/modules/login/error-constants.js
+++ src/modules/login/error-constants.js
@@ -1,12 +1,9 @@
-const loginInvalid = "email must be a valid email";
+const loginInvalid = "invalid login";
 const emailRequired = "email required";
 const passwordRequired = "password required";
-
-const confirmEmail = "email has not been verified";
 
 module.exports = {
 	loginInvalid,
 	emailRequired,
-	passwordRequired,
-	confirmEmail
+	passwordRequired
 }
